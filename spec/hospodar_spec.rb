fbf5cfda68fb656202d6a12ea6fda74c9394ab99
Andrii Baran <andriy.baran.v@gmail.com>
2021-11-04 11:08:48 +0200

    Initial commit

---
+++ spec/hospodar_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Hospodar do
+  it "has a version number" do
+    expect(Hospodar::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
